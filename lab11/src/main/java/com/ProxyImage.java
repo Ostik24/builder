2890d401a04f9ab34c8164e2b775cf7b515d81fd
Ostap Pavlyshyn <ostappavlishin@gmail.com>
2024-12-28 17:33:49 +0200

    fixing

--- lab11/src/main/java/com/ProxyImage.java
+++ lab11/src/main/java/com/ProxyImage.java
@@ -16,21 +16,3 @@ public class ProxyImage implements MyImage {
         realImage.display();
     }
 }
-
-class RealImage implements MyImage {
-    private String filename;
-
-    public RealImage(String filename) {
-        this.filename = filename;
-        loadFromDisk();
-    }
-
-    private void loadFromDisk() {
-        System.out.println("Loading image: " + filename);
-    }
-
-    @Override
-    public void display() {
-        System.out.println("Displaying image: " + filename);
-    }
-}
--- lab11/src/main/java/com/RealImage.java
+++ lab11/src/main/java/com/RealImage.java
@@ -1,13 +1,28 @@
 package com;
 
+import java.awt.GraphicsEnvironment;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class RealImage implements MyImage {
+    private String filename;
     private JFrame frame;
 
     public RealImage(String filename) {
+        this.filename = filename;
+        if (GraphicsEnvironment.isHeadless()) {
+            loadFromDisk();
+        } else {
+            setupGUI();
+        }
+    }
+
+    private void loadFromDisk() {
+        System.out.println("Loading image: " + filename);
+    }
+
+    private void setupGUI() {
         frame = new JFrame();
         ImageIcon icon = new ImageIcon(filename);
         JLabel label = new JLabel(icon);
@@ -16,7 +31,12 @@ public class RealImage implements MyImage {
         frame.pack();
     }
 
+    @Override
     public void display() {
-        frame.setVisible(true);
+        if (frame != null) {
+            frame.setVisible(true);
+        } else {
+            System.out.println("Displaying image: " + filename);
+        }
     }
 }
